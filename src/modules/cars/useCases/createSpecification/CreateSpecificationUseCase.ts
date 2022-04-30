07772ac3cb3909c20c164bdf05b23aa9d3df8b8f
Samuel Damasceno Filho <samuelfilho@outlook.com>
2022-04-29 22:26:16 -0300

    Criado migration para especificações, adicionado typeorm na entidade de epecificações e refatorado repositorio, controller e use cases de espcificações

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -14,15 +14,15 @@ class CreateSpecificationUseCasa {
     private specificationsRepository: ISpecificationsRepository
   ) {}
 
-  execute({ name, description }: IRequest): void {
+  async execute({ name, description }: IRequest): Promise<void> {
     const specificationAlreadyExists =
-      this.specificationsRepository.findByName(name);
+      await this.specificationsRepository.findByName(name);
 
     if (specificationAlreadyExists) {
       throw new Error("Specification already exists.");
     }
 
-    this.specificationsRepository.create({ name, description });
+    await this.specificationsRepository.create({ name, description });
   }
 }
 
