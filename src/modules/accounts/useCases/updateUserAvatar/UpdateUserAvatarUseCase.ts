d10a87024f075e2049644c68ca79b9fb5f8b8d6e
Samuel Damasceno Filho <samuelfilho@outlook.com>
2022-05-01 16:48:10 -0300

    Adicionado upload de avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+  avatar_File: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository") private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ user_id, avatar_File }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
+
+    user.avatar = avatar_File;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
