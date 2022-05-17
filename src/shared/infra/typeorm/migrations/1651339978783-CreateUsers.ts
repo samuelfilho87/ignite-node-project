f9e45ec3683979238c697605f7f1a29b603b9f13
Samuel Damasceno Filho <samuelfilho@outlook.com>
2022-05-16 21:04:37 -0300

    Migration rentals

--- src/shared/infra/typeorm/migrations/1651339978783-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1651339978783-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1651339978783 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
