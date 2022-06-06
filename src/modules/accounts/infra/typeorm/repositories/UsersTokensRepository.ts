7326bd22ae823ba20373d53371ac9bf80244dc50
Samuel Damasceno Filho <samuelfilho@outlook.com>
2022-06-06 20:01:43 -0300

    Refresh token, entity and repository

---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,22 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+
+import { UserTokens } from "../entities/UserTokens";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+  private repository: Repository<UserTokens>;
+
+  constructor() {
+    this.repository = getRepository(UserTokens);
+  }
+
+  async create(data: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = this.repository.create(data);
+
+    return this.repository.save(userToken);
+  }
+}
+
+export { UsersTokensRepository };
